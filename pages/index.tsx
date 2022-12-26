4e8cc358523ad2a2f194a4e1bf74650ebe903797
Jaelson Santos <jaelson.jua@gmail.com>
2022-12-26 18:02:43 -0300

    fix: default pendriver size

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ interface HomeProps {
 }
 
 export default function Home({ games: gamesContent }: HomeProps) {
-  const [pendriverSize, setPendriverSize] = useState("4e+9");
+  const [pendriverSize, setPendriverSize] = useState("8589934592");
   const toast = useToast();
 
   const [gamesSelecteds, setGamesSelecteds] = useState<Game[]>([]);
